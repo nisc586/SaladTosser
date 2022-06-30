d514a94aacfb1c0d2ce422f426f5abb9cb63da84
nisc586 <49026680+nisc586@users.noreply.github.com>
2022-06-30 21:29:12 +0200

    Spawn 3 salad leaves at a time

--- Main.gd
+++ Main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # Declare member variables here. Examples:
-export (PackedScene) var ingredient_scene
+export (PackedScene) var salad
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,14 +10,26 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# func _process(delta):
+# 	pass
 
 
 func _on_IngredientSpawnTimer_timeout():
 	if not Input.is_action_pressed("click"):
 		return
 
-	var ingredient = ingredient_scene.instance()
-	ingredient.position = get_viewport().get_mouse_position()
+	var spawn_position = get_viewport().get_mouse_position()
+	var offset = 100
+	var ingredient = salad.instance()
+	ingredient.position = spawn_position
 	add_child(ingredient)
+	
+	ingredient = salad.instance()
+	ingredient.position = spawn_position + Vector2.LEFT * offset
+	add_child(ingredient)
+	
+	ingredient = salad.instance()
+	ingredient.position = spawn_position + Vector2.RIGHT * offset
+	add_child(ingredient)
+	
+	print()
