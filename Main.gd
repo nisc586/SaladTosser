91db3ee3dacd0feb5f3ff4fe00e117305906e2c0
nisc586 <49026680+nisc586@users.noreply.github.com>
2022-07-06 23:11:11 +0200

    Add scene for player, update spawning of salad

--- Main.gd
+++ Main.gd
@@ -6,30 +6,36 @@ export (PackedScene) var salad
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-# 	pass
+func _process(_delta):
+	var hand = $PlayerHand
+	hand.position = get_viewport().get_mouse_position()
 
 
 func _on_IngredientSpawnTimer_timeout():
 	if not Input.is_action_pressed("click"):
 		return
 
-	var spawn_position = get_viewport().get_mouse_position()
-	var offset = 100
-	var ingredient = salad.instance()
-	ingredient.position = spawn_position
-	add_child(ingredient)
+#	var spawn_position = get_viewport().get_mouse_position()
+#	var offset = 100
+#	var ingredient = salad.instance()
+#	ingredient.position = spawn_position
+#	add_child(ingredient)
+#
+#	ingredient = salad.instance()
+#	ingredient.position = spawn_position + Vector2.LEFT * offset
+#	add_child(ingredient)
+#
+#	ingredient = salad.instance()
+#	ingredient.position = spawn_position + Vector2.RIGHT * offset
+#	add_child(ingredient)
 	
-	ingredient = salad.instance()
-	ingredient.position = spawn_position + Vector2.LEFT * offset
-	add_child(ingredient)
 	
-	ingredient = salad.instance()
-	ingredient.position = spawn_position + Vector2.RIGHT * offset
-	add_child(ingredient)
-	
-	print()
+	var nossle = $PlayerHand/LineNossle
+	for child in nossle.get_children():
+		var ingredient = salad.instance()
+		ingredient.position = child.get_global_position()
+		add_child(ingredient)
