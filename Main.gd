01161d9d34f56f3e769ebdc948d50f36d946ee61
nisc586 <49026680+nisc586@users.noreply.github.com>
2022-07-16 22:17:48 +0200

    Set mouse position to middle of screen at game start

--- Main.gd
+++ Main.gd
@@ -7,6 +7,7 @@ export (PackedScene) var salad
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	get_viewport().warp_mouse($StartingPosition.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
